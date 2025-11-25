bd78276fbca5319fea8609fb53b33a8b7d88c76e
chcosta <carloshf377@gmail.com>
2025-11-24 21:24:51 -0300

    refatorando task.py para privar tarefas por usuario

--- models/task.py
+++ models/task.py
@@ -1,16 +1,19 @@
-#CAMADA DE MODELOS
+# CAMADA DE MODELOS
 class Task:
-    def __init__(self, title, description, status, id=None):
+    # Adicionamos 'user_id' nos parâmetros do construtor
+    def __init__(self, title, description, status, user_id=None, id=None):
         self.id = id
         self.title = title
         self.description = description
         self.status = status # "todo", "doing", "done"
+        self.user_id = user_id # Novo atributo para identificar o dono
 
-    #essencial para serializar o objeto para a API
+    # Essencial para serializar o objeto para a API
     def to_dict(self):
         return {
             "id": self.id,
             "title": self.title,
             "description": self.description,
-            "status": self.status
+            "status": self.status,
+            "user_id": self.user_id # Incluímos o ID no JSON (útil para debug)
         }
